62ace6759f4a71b723e462cfcbe37f1e333bfbe3
Ricardo Koci <arsoftware10@gmail.com>
2022-01-09 14:48:55 +0000

    eu te amo...

--- bw_compression_research/linked_list1_dl.h
+++ bw_compression_research/linked_list1_dl.h
@@ -7,16 +7,19 @@ bool is_it_the_first_byte_to_encode_dl;
 void __fastcall convert_8_bits_to_nine_bits(__attribute__((unused)) int8_t *input_mem_dl,
 											__attribute__((unused)) int len_of_input_to_encode_as_you_may_expect_dl,
 											__attribute__((unused)) int8_t *output_mem_dl,
-											__attribute__((unused)) int *bytes_encoded_so_far_dl, // a contagem é o tamanho dos dados ric, só isso, nao precisa processar duas vezes a cada processo vai salvando os dados que precisa, só precisa dar uma passagem rapida pra saber o tamanho dos dados, ok...
-											__attribute__((unused)) int8_t *montagem_dl_interno,
-											__attribute__((unused)) int8_t *bitposition_dl_interno_for_your_pleasure__)
+											__attribute__((unused)) int *bytes_encoded_so_far_dl, // a contagem é o tamanho dos dados ric, só isso, nao precisa processar duas vezes a cada processo vai salvando os dados que precisa, só precisa dar uma passagem rápida pra saber o tamanho dos dados, ok...
+											__attribute__((unused)) int8_t *montagem_dl_interno)
 {
 
 	// int bitcount__dl = 0;
 	// int16_t temp_16_dl;
-	int8_t *ptr_dl = input_mem_dl;
+	int deslocado_2_dl;
+
 	int i_dl;
+
 	static int16_t meu_buf_16_dl[512];
+
+	int16_t *ptr_16_dl = meu_buf_16_dl;
 
 	for (i_dl = 0; i_dl < len_of_input_to_encode_as_you_may_expect_dl; i_dl++)
 	{
@@ -29,7 +32,7 @@ void __fastcall convert_8_bits_to_nine_bits(__attribute__((unused)) int8_t *inpu
 	{
 		is_it_the_first_byte_to_encode_dl = false;
 		bitposition_dl = 0;
-		(*bitposition_dl_interno_for_your_pleasure__) = 0;
+		//(*bitposition_dl_interno_for_your_pleasure__) = 0;
 		montagem_dl = 0;
 		deslocador_dl = 0;
 		(*montagem_dl_interno) = 0;
@@ -37,219 +40,215 @@ void __fastcall convert_8_bits_to_nine_bits(__attribute__((unused)) int8_t *inpu
 
 	while (len_of_input_to_encode_as_you_may_expect_dl--)
 	{
+		deslocado_2_dl = 9;
 
-		switch (bitposition_dl)
+		while (deslocado_2_dl--)
 		{
-		case 0:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
+			switch (bitposition_dl)
 			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
+			case 0:
 
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-			bitposition_dl++;
-			break;
-		case 1:
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
 
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 1:
 
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 2:
+
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 3:
+
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 4:
+
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 5:
+
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 6:
+
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 7:
+
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl++;
+				break;
+			case 8:
+
+				if ((1 << bitposition_dl) & *ptr_16_dl) // aqui agira vai ate 9
+				{
+					montagem_dl |= (1 << deslocador_dl);
+				}
+				else
+				{
+					montagem_dl &= ~(1 << deslocador_dl);
+				}
+				(*montagem_dl_interno) = montagem_dl;
+				deslocador_dl++;
+
+				if (8 == deslocador_dl)
+				{
+					(*bytes_encoded_so_far_dl)++;
+					(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro..., ja foi prealocado do tamanho certo com multiplicacao por .2
+					output_mem_dl++;
+					deslocador_dl = 0;
+				}
+				bitposition_dl = 0;
+				break;
 			}
-
-			bitposition_dl++;
-			break;
-		case 2:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
-
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-			bitposition_dl++;
-			break;
-		case 3:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
-
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-
-			bitposition_dl++;
-			break;
-		case 4:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
-
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-
-			bitposition_dl++;
-			break;
-
-		case 5:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
-
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-
-			bitposition_dl++;
-			break;
-		case 6:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
-
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-
-			bitposition_dl++;
-			break;
-
-		case 7:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
-
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-
-			bitposition_dl++;
-			break;
-		case 8:
-
-			if ((1 << deslocador_dl) & *ptr_dl++)
-			{
-				montagem_dl |= (1 << deslocador_dl);
-			}
-			else
-			{
-				montagem_dl &= ~(1 << deslocador_dl);
-			}
-			(*montagem_dl_interno) = montagem_dl;
-			deslocador_dl++;
-
-			if (8 == deslocador_dl)
-			{
-				(*bytes_encoded_so_far_dl)++;
-				(*output_mem_dl) = montagem_dl; // quando encher um byte inteiro...
-				output_mem_dl++;
-				deslocador_dl = 0;
-			}
-
-			bitposition_dl = 0;
-			break;
 		}
+		ptr_16_dl++;
 	}
-	(*bitposition_dl_interno_for_your_pleasure__) = bitposition_dl; // next bit to set when reconstructing the data
+	//(*bitposition_dl_interno_for_your_pleasure__) = bitposition_dl; // next bit to set when reconstructing the data
 	return;
 }
 
@@ -262,7 +261,9 @@ struct my_struct_for_list_ar_is__dl__update_dl // for list only
 	char *filename_k;
 	int last_item_of_nine_bits_encode_dl; // complexo mas tudo bem, da para lidar mais tarde otimizamos isso, codifica tudo com 9 bits
 	int bytes_encoded_so_far_dl;
-	char *memory_for_string_dl;
+	int8_t montagem_dl_interno;
+	int8_t *memory_for_string_dl;
+	int8_t *output_memory_for_string_dl;
 	int len_of_memory_dl;
 	int number_of_index_in_linked_list_dl;		 // if in linked list only need this, dont you agree?
 	bool true_if_is_entry_in_the_linked_list_dl; // redundant but we will keep it...because if the memory is on it is not linked list, ok?
@@ -293,24 +294,33 @@ void add_more_one_is__dl__update_dl(__attribute__((unused)) uint8_t *memory_for_
 		aak_inicio_is__dl__update_dl = aak_is__dl__update_dl;
 		aak_pointer_is__dl__update_dl = aak_is__dl__update_dl;
 
-		aak_is__dl__update_dl->len_of_memory_dl = len_of_memory_dl;
+		// aak_is__dl__update_dl->len_of_memory_dl = len_of_memory_dl;
 
 		aak_is__dl__update_dl->number_of_index_in_linked_list_dl = number_of_index_in_linked_list_dl;
 
 		aak_is__dl__update_dl->true_if_is_entry_in_the_linked_list_dl = true_if_is_entry_in_the_linked_list_dl;
 
-		if (memory_for_string_dl)
+		if (false == true_if_is_entry_in_the_linked_list_dl)
 		{
 
 			d_temp_dl = (double)len_of_memory_dl;
 
 			d_temp_dl = d_temp_dl * .2;
 
-			aak_is__dl__update_dl->memory_for_string_dl = malloc((int)d_temp_dl);
+			aak_is__dl__update_dl->output_memory_for_string_dl = malloc((int)d_temp_dl);
+			aak_is__dl__update_dl->memory_for_string_dl = malloc(len_of_memory_dl);
 			memcpy(aak_is__dl__update_dl->memory_for_string_dl, memory_for_string_dl, len_of_memory_dl);
+
+			convert_8_bits_to_nine_bits(aak_is__dl__update_dl->memory_for_string_dl,
+										len_of_memory_dl,
+										aak_is__dl__update_dl->output_memory_for_string_dl,
+										&aak_is__dl__update_dl->bytes_encoded_so_far_dl,
+										&aak_is__dl__update_dl->montagem_dl_interno);
+
+			assert(aak_is__dl__update_dl->bytes_encoded_so_far_dl < (int)d_temp_dl);
 		}
 		else
-			aak_is__dl__update_dl->memory_for_string_dl = NULL;
+			aak_is__dl__update_dl->memory_for_string_dl = NULL; // later...
 
 		aak_is__dl__update_dl->next_ar = calloc(1, sizeof(struct my_struct_for_list_ar_is__dl__update_dl));
 
@@ -334,13 +344,31 @@ void add_more_one_is__dl__update_dl(__attribute__((unused)) uint8_t *memory_for_
 
 		aak_ptr->true_if_is_entry_in_the_linked_list_dl = true_if_is_entry_in_the_linked_list_dl;
 
-		if (memory_for_string_dl)
+		aak_ptr->number_of_index_in_linked_list_dl = number_of_index_in_linked_list_dl;
+
+		aak_ptr->true_if_is_entry_in_the_linked_list_dl = true_if_is_entry_in_the_linked_list_dl;
+
+		if (false == true_if_is_entry_in_the_linked_list_dl)
 		{
+
+			d_temp_dl = (double)len_of_memory_dl;
+
+			d_temp_dl = d_temp_dl * .2;
+
+			aak_ptr->output_memory_for_string_dl = malloc((int)d_temp_dl);
 			aak_ptr->memory_for_string_dl = malloc(len_of_memory_dl);
-			memcpy(aak_ptr->memory_for_string_dl, memory_for_string_dl, len_of_memory_dl); // dont need plus one because it is memory not string, ok?
+			memcpy(aak_ptr->memory_for_string_dl, memory_for_string_dl, len_of_memory_dl);
+
+			convert_8_bits_to_nine_bits(aak_ptr->memory_for_string_dl,
+										len_of_memory_dl,
+										aak_ptr->output_memory_for_string_dl,
+										&aak_ptr->bytes_encoded_so_far_dl,
+										&aak_ptr->montagem_dl_interno);
+
+			assert(aak_ptr->bytes_encoded_so_far_dl < (int)d_temp_dl);
 		}
 		else
-			aak_ptr->memory_for_string_dl = NULL;
+			aak_ptr->memory_for_string_dl = NULL; // later...
 
 		aak_is__dl__update_dl = aak_ptr->next_ar;
 		has_itens_is__dl__update_dl++;
