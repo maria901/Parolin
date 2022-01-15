f2545f2e85ee57507f8909099158e3ca6905aeac
Ricardo Koci <arsoftware10@gmail.com>
2022-01-15 13:41:22 +0000

    now pure dl_compressor research (2022)

---
+++ bw_compression_research/adler32/dl/ric_dl.c
@@ -0,0 +1,101 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*                                                                             *
+*        Licensa de Cópia (C) <2022>  <Corporação do Trabalho Binário>        *
+*                                                                             *
+*     Este  programa  é software livre: você pode redistribuir isto e/ou      *
+*     modificar  isto sobre os termos do  GNU Licensa Geral Pública como     10
+*     publicado  pela Fundação  de Software  Livre, tanto a versão 3  da      *
+*     Licensa, ou (dependendo da sua opção) qualquer versão posterior.        *
+*                                                                             *
+*     Este  programa é distribuído na  esperança que isto vai  ser útil,      *
+*     mas SEM  QUALQUER GARANTIA; sem  até mesmo a implicada garantia de      *
+*     COMERCIALIZAÇÃO ou CABIMENTO PARA UM FIM PARTICULAR.  Veja a            *
+*     Licensa Geral Pública para mais detalhes.                               *
+*                                                                             *
+*     Você deve ter recebido uma  cópia da LICENSA GERAL PUBLICA e a GNU      *
+*     Licensa Pública Menor junto com este programa                           *
+*     Se não, veja <http://www.gnu.org/licenses/>.                            *
+*                                                                             *
+*     Suporte: https://nomade.sourceforge.io/                                 *
+*                                                                             *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+     E-mails:
+     maria@arsoftware.net.br
+     pedro@locacaodiaria.com.br
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*                                                                             *
+*     contato imediato(para uma resposta muito rápida) WhatsApp               *
+*     (+55)41 9627 1708 - isto está sempre ligado (eu acho...)                *
+*                                                                             *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  */
+
+// ric dl - Última modificação/compilação: Tue Jan 11 11:51 2022
+
+#include <windows.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <math.h>
+#include <wctype.h>
+#include <wchar.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <locale.h>
+#include <signal.h>
+#include <limits.h>
+#include <float.h>
+#include <iso646.h>
+
+#define NDEBUG
+#include <assert.h>
+
+#include <stdbool.h>
+
+#include <process.h>
+
+#ifndef uchar
+#define uchar unsigned char
+#endif
+
+#ifndef uint
+#define uint unsigned int
+#endif
+
+#ifndef ushort
+#define ushort unsigned short
+#endif
+
+// ric bread... now code
+
+/*
+
+you need to read the file, and make a copy of the file, in memory to avoid problems...
+
+1 - read the file line by line
+
+
+
+ */
+
+int main(__attribute__((unused)) int argc_ric, __attribute__((unused)) char **argv_ric)
+{
+
+     int return_value_dl = 0;
+FILE * my_stolen_c_file_to_patch_by_ric_the_guy_dl
+     if (2 != argc_ric)
+     {
+          MessageBoxA(NULL, "Ric, missing the argument, you idiot...", "Code Stolen from stackoverflow", 0);
+          return_value_dl = 30;
+          goto sai_ric_diz_dl;
+     }
+sai_ric_diz_dl:;
+     return return_value_dl;
+}
